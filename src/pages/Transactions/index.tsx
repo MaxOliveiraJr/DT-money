d4021d62d395ad836f8d02f9be95f15f65f161bc
Max Oliveira <max-oliveira@outlook.com.br>
2025-05-06 22:29:49 -0300

    feat: page transaction

---
+++ src/pages/Transactions/index.tsx
@@ -0,0 +1,46 @@
+import { Header } from "../../components/Header";
+import { Summary } from "../../components/Summary";
+import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
+
+export function Transactions() {
+    return <div>
+        <Header />
+        <Summary />
+        <TransactionsContainer>
+            <TransactionsTable>
+                <tbody>
+                    <tr>
+                        <td width={"50%"}>Desenvolvimento de site</td>
+                        <td>
+                            <PriceHighlight variant="income">
+                                R$ 12.000,00
+                            </PriceHighlight>
+                        </td>
+                        <td>Cartão</td>
+                        <td>15/03/2022</td>
+                    </tr>
+                    <tr>
+                        <td width={"50%"}>Desenvolvimento de site</td>
+                        <td>
+                            <PriceHighlight variant="income">
+                                R$ 12.000,00
+                            </PriceHighlight>
+                        </td>
+                        <td>Cartão</td>
+                        <td>15/03/2022</td>
+                    </tr>
+                    <tr>
+                        <td width={"50%"}>Desenvolvimento de site</td>
+                        <td>
+                            <PriceHighlight variant="income">
+                                R$ 12.000,00
+                            </PriceHighlight>
+                        </td>
+                        <td>Cartão</td>
+                        <td>15/03/2022</td>
+                    </tr>
+                </tbody>
+            </TransactionsTable>
+        </TransactionsContainer>
+    </div>
+}
