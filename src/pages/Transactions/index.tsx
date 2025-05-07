42e62fd676acff00638d682e00db3a6c3c06d4a1
Max Oliveira <max-oliveira@outlook.com.br>
2025-05-07 19:23:23 -0300

    feat: add context for transactions fetch

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,32 +1,15 @@
-import { useEffect, useState } from "react";
+import { useContext } from "react";
 import { Header } from "../../components/Header";
 import { SearchForm } from "../../components/SearchForm";
 import { Summary } from "../../components/Summary";
 import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
+import { TransactionsContext } from "../../contexts/TransactionsContext";
 
-
-interface Transaction {
-    id: number;
-    description: string;
-    type: "income" | "outcome";
-    price: number;
-    category: string;
-    createdAt: string;
-}
 export function Transactions() {
 
-    const [transactions, setTransactions] = useState<Transaction[]>([])
 
+    const { transactions } = useContext(TransactionsContext)
 
-    async function loadTransactions() {
-        const response = await fetch("http://localhost:3000/transactions")
-        const data = await response.json()
-
-        setTransactions(data);
-    }
-    useEffect(() => {
-        loadTransactions();
-    }, [])
 
     return <div>
         <Header />
@@ -35,7 +18,6 @@ export function Transactions() {
             <SearchForm />
             <TransactionsTable>
                 <tbody>
-
                     {transactions.map(transaction => {
                         return (
                             <tr key={transaction.id}>
