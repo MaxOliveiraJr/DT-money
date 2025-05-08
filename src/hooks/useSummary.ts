aea2ea2d4262d5d7aec58fd47239d11b2485e185
Max Oliveira <max-oliveira@outlook.com.br>
2025-05-08 20:10:06 -0300

    refacor: add useMemo in variable to performance

--- src/hooks/useSummary.ts
+++ src/hooks/useSummary.ts
@@ -1,3 +1,4 @@
+import { useMemo } from "react";
 import { TransactionsContext } from "../contexts/TransactionsContext";
 import { useContextSelector } from "use-context-selector";
 
@@ -6,24 +7,26 @@ export function useSummary() {
     return context.transactions;
   });
 
-  const summary = transactions.reduce(
-    (acc, transtion) => {
-      if (transtion.type === "income") {
-        acc.income += transtion.price;
-        acc.total += transtion.price;
-      } else {
-        acc.outcome += transtion.price;
-        acc.total -= transtion.price;
-      }
+  const summary = useMemo(() => {
+    return transactions.reduce(
+      (acc, transtion) => {
+        if (transtion.type === "income") {
+          acc.income += transtion.price;
+          acc.total += transtion.price;
+        } else {
+          acc.outcome += transtion.price;
+          acc.total -= transtion.price;
+        }
 
-      return acc;
-    },
-    {
-      income: 0,
-      outcome: 0,
-      total: 0,
-    }
-  );
+        return acc;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      }
+    );
+  }, [transactions]);
 
   return summary;
 }
